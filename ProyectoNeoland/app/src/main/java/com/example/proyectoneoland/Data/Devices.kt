e86da8813cab88a0b74bf3e09832f0c1c6465bee
Luis Villanueva <luisvillanueva97@gmail.com>
2020-09-11 09:28:09 +0200

    Init

---
+++ ProyectoNeoland/app/src/main/java/com/example/proyectoneoland/Data/Devices.kt
@@ -0,0 +1,31 @@
+package com.example.proyectoneoland.Data
+
+import androidx.room.Entity
+
+@Entity
+data class Devices(
+    var name: String,
+    var owner: String,
+    var pictures: Picture,
+    var widgets: Widget,
+    var toggle: Boolean,
+    var type: DeviceType
+) {
+    enum class DeviceType {
+
+        Light,
+        Speaker,
+        Outlet
+
+    }
+}
+
+data class Picture(
+    val pictureOn: Int,
+    val pictureOff: Int
+)
+
+data class Widget(
+    val widgetOn: Int,
+    val widgetOff: Int
+)
---
+++ ProyectoNeoland/app/src/main/java/com/example/proyectoneoland/Data/DevicesDao.kt
@@ -0,0 +1,27 @@
+package com.example.proyectoneoland.Data
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+
+@Dao
+interface DevicesDao {
+
+    @Query("SELECT * FROM Devices")
+    fun getAll() : List<Devices>
+
+    @Insert
+    fun insert(Devices : Devices)
+
+    @Insert
+    fun insertAll(Devices : List<Devices>)
+
+    @Update
+    fun update(Devices : Devices)
+
+    @Delete
+    fun delete(Devices: Devices)
+
+    @Query("SELECT * FROM Devices")
+    fun getAllLive() : LiveData<List<Devices>>
+}
